3fb4ff6065b383d51ff94f691201b68dce836045
Christian Esswein <c.esswein@gmail.com>
2015-05-12 11:11:15 +0200

    allow cross-origin access

--- build/grunt-tasks/connect.js
+++ build/grunt-tasks/connect.js
@@ -1,11 +1,27 @@
-module.exports = function(grunt, data) {
+module.exports = function(grunt, data){
 
-    return {
-        server: {
-            options: {
-                port: 9000,
-                hostname: 'localhost'
-            }
+  return {
+    server:
+    {
+      options:
+      {
+        port: 9000,
+        hostname: 'localhost',
+
+        middleware: function(connect, options, middlewares)
+        {
+          // allow cross origin access
+          middlewares.unshift(function(req, res, next)
+          {
+            res.setHeader('Access-Control-Allow-Credentials', true);
+            res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+            next();
+          });
+
+          return middlewares;
         }
-    };
+      }
+    }
+  };
 }
